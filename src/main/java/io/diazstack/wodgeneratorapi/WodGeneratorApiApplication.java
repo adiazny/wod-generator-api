437867da4ae2e7740b30d8a4000c8528abaa2bb2
Alan Diaz <adiazny@gmail.com>
2020-07-17 21:42:05 -0400

    Initial commit

---
+++ src/main/java/io/diazstack/wodgeneratorapi/WodGeneratorApiApplication.java
@@ -0,0 +1,13 @@
+package io.diazstack.wodgeneratorapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WodGeneratorApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WodGeneratorApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/diazstack/wodgeneratorapi/WodGeneratorApiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.diazstack.wodgeneratorapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WodGeneratorApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
